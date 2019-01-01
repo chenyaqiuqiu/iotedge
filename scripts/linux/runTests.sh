83b207f1cc58bff73fb76a92d980cce46c723912
Philip Lin <philipktlin@live.com>
2018-12-31 17:30:51 -0800

    clean up test script

--- scripts/linux/runTests.sh
+++ scripts/linux/runTests.sh
@@ -38,26 +38,12 @@ if [ ! -d "$BUILD_BINARIESDIRECTORY" ]; then
   exit 1
 fi
 
-echo "Running tests in all test projects with filter: ${TEST_FILTER#--filter }"
+testFilterValue="${TEST_FILTER#--filter }"
+echo "Running tests in all test projects with filter: $testFilterValue"
 
 RES=0
-#while read proj; do
-#  echo "Running tests for project - $proj"
-#  TESTENVIRONMENT=$ENVIRONMENT $DOTNET_ROOT_PATH/dotnet test \
-#    $TEST_FILTER \
-#    -p:ParallelizeTestCollections=false \
-#    --no-build \
-#    -v d \
-#    --logger "trx;LogFileName=result.trx" \
-#    -o "$OUTPUT_FOLDER" \
-#    $proj
-#  if [ $? -gt 0 ]
-#  then
-#    RES=1
-#    echo "Error running test $proj, RES = $RES"
-#  fi
-#done < <(find $ROOTFOLDER -type f -iname $SUFFIX)
 
+# Find all test project dlls
 testProjectDlls = ""
 while read proj; do
   fileParentDirectory="$(dirname -- "$proj")"
@@ -71,9 +57,12 @@ done < <(find $ROOTFOLDER -type f -iname $SUFFIX)
 
 echo "test project dlls:$testProjectDlls"
 
-# dotnet vstest "$BUILD_REPOSITORY_LOCALPATH/edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Core.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Agent.Core.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Docker.E2E.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Agent.Docker.E2E.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Docker.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Agent.Docker.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Edgelet.Docker.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Agent.Edgelet.Docker.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Edgelet.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Agent.Edgelet.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Agent.IoTHub.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Amqp.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.Amqp.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.CloudProxy.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.Core.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.E2E.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.E2E.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Http.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.Http.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Mqtt.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.Mqtt.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Service.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Hub.Service.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-hub/test/Microsoft.Azure.Devices.Routing.Core.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Routing.Core.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-util/test/Microsoft.Azure.Devices.Edge.Storage.RocksDb.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Storage.RocksDb.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-util/test/Microsoft.Azure.Devices.Edge.Storage.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Storage.Test.dll" "$BUILD_REPOSITORY_LOCALPATH/edge-util/test/Microsoft.Azure.Devices.Edge.Util.Test/bin/Release/netcoreapp2.1/Microsoft.Azure.Devices.Edge.Util.Test.dll" /TestCaseFilter:"Category=Integration&Category!=Stress" /Logger:"trx" /TestAdapterPath:"$BUILD_REPOSITORY_LOCALPATH" /Parallel
-
-dotnet vstest $testProjectDlls /TestCaseFilter:"Category=Integration&Category!=Stress" /Logger:"trx" /TestAdapterPath:"$BUILD_REPOSITORY_LOCALPATH" /Parallel
+if [ -z "$testFilterValue" ]
+then
+      dotnet vstest $testProjectDlls /Logger:"trx" /TestAdapterPath:"$BUILD_REPOSITORY_LOCALPATH" /Parallel
+else
+      dotnet vstest $testProjectDlls /TestCaseFilter:"$testFilterValue" /Logger:"trx" /TestAdapterPath:"$BUILD_REPOSITORY_LOCALPATH" /Parallel
+fi
 
 if [ $? -gt 0 ]
 then
